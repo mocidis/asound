859756c286ec38c384893e2642204060b8b24ac3
thanhnt <thanhnt.k56@gmail.com>
2015-12-14 16:23:29 +0700

    udpate

--- build.sh
+++ build.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
+if [ $# -lt 1 ]; then
+echo "
+Usage:
+    ./build.sh [0|1]
+        0: don't do cross compile for arm
+        1: do cross compile for arm
+"
+exit
+fi
 #define constant variable
 LINUX_ARMV7L=$PWD/../libs/linux-armv7l
 LINUX_X86_64=$PWD/../libs/linux-x86_64
 LINUX_I686=$PWD/../libs/linux-i686
 MINGW=$PWD/../libs/mingw32-i586
 MACOS=$PWD/../libs/darwin-x86_64
-
+ARM=$1
 #MACOS
 uname -a | grep "Darwin"
 if [ $? == 0 ]; then
@@ -29,11 +38,12 @@ fi
 
 echo $INSTALL_DIR
 #build for arm first
-make distclean
-./configure --host=arm-linux --disable-python --prefix=$LINUX_ARMV7L CC=arm-none-linux-gnueabi-gcc
-make -j4
-make install
-
+if [ $ARM == 1 ]; then
+	make distclean
+	./configure --host=arm-linux --disable-python --prefix=$LINUX_ARMV7L CC=arm-none-linux-gnueabi-gcc
+	make -j4
+	make install
+fi
 #build for intel
 make distclean
 ./configure --disable-python --prefix=$INSTALL_DIR
